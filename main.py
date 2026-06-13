8a9b0cbaa85a0da9f274f18e254c366a981c767c
Youssef El <53300987+ucef-el@users.noreply.github.com>
2026-06-13 23:48:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import smtplib
 import os
 
 MY_EMAIL = os.environ.get("MY_EMAIL")
-PASSWORD = os.environ.get("PASSWORD")
+PASSWORD = os.environ.get("My_PASSWORD")
 APPID =  os.environ.get("APPID")
 
 
